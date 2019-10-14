9128389fbd775a0da73bc41f2044d256c3f57500
Luis Enrique Navarrete MAC <luis.e.415@gmail.com>
2019-10-14 09:56:33 -0500

    Nuevas versiones

--- Javascript/23-funciones-texto.js
+++ Javascript/23-funciones-texto.js
@@ -2,7 +2,7 @@
 
 //Transformación de textos
 var numero = 333;
-var texto1 = "Bienvenido al curso de JS de Victor Robles";
+var texto1 = "  Bienvenido al curso de JS de Victor Robles  ";
 var texto2 = "Es muy buen curso";
 
 
@@ -25,6 +25,40 @@ console.log(textoTotal);
 
 
 //METODOS DE BUSQUEDA
+
+//indexOf - primera coincidencia de la palabra
+console.log("Busqueda Index of:");
+var busqueda = texto1.indexOf("curso");
+console.log(busqueda);
+
+//Search
+console.log("Busqueda Search:");
+var busqueda = texto1.search("curso");
+console.log(busqueda);
+
+//charAt
+console.log("Busqueda charAt:");
+var busqueda = texto1.charAt("20");
+console.log(busqueda);
+
+//METOODOS DE REEMPLAZO
+
+//replace
+console.log("Reemplazo replace:");
+var busqueda = texto1.replace("curso", "Cursinis");
+console.log(busqueda);
+
+
+//slice
+console.log("Reemplazo split:");
+var busqueda = texto1.split(" ");
+console.log(busqueda);
+
+//trim
+console.log("Reemplazo trim:");
+var busqueda = texto1.trim();
+console.log(busqueda);
+
 
 
 
---
+++ Javascript/24-plantilla.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//Plantillas de texto
+
+var nombre = prompt("Dame tu nombre: ");
+var apellidos = prompt("Dame tu apellido");
+
+var texto = "Tu nombre es: " + nombre + " "+ apellidos;
+
+//Escribimos respuesta en documento: 
+document.write(texto);
